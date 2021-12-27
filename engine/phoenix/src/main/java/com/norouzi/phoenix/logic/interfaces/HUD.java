0fc5859e351a67c91693c2d60adc30887f443c4b
Ali Norouzi <alinorouzi79@gmail.com>
2021-12-27 16:02:37 +0330

    [Ali Norouzi] enhanced the health bar

--- engine/phoenix/src/main/java/com/norouzi/phoenix/logic/interfaces/HUD.java
+++ engine/phoenix/src/main/java/com/norouzi/phoenix/logic/interfaces/HUD.java
@@ -6,15 +6,18 @@ import java.awt.*;
 
 public class HUD {
     public static int HEALTH = 100;
+    private int greenValue = 255;
 
     public void tick() {
         HEALTH = Game.clamp(HEALTH, 0, 100);
+        greenValue = Game.clamp(greenValue, 0, 255);
+        greenValue = HEALTH * 2;
     }
 
     public void render(Graphics g) {
         g.setColor(Color.GRAY);
         g.fillRect(15, 15, 200, 32);
-        g.setColor(Color.GREEN);
+        g.setColor(new Color(75, greenValue, 0));
         g.fillRect(15, 15, HEALTH * 2, 32);
         g.setColor(Color.WHITE);
         g.drawRect(15, 15, 200, 32);
